448cdb975455bef2dfe412be722c97f9a21aaea7
user <sivtsovatanya1985@gmail.com>
2022-04-06 20:47:24 +0300

    Created new project list:
    - added an ability for adding new person and seeing into console

--- src/components/person-list/person-list-item/PersonItem.js
+++ src/components/person-list/person-list-item/PersonItem.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import '../person-list-item/PersonItem.css';
 import Person from "../person";
 import Cards from "../cards/cards";
+import PersonFilter from "../../filters/PersonFilter";
 
 const PersonItem = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2021');
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+        console.log(selectedYear);
+    }
+
     return (
         <Cards className="list">
+            <PersonFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             <Person date={props.items[0].date} name={props.items[0].name} age={props.items[0].age}></Person>
             <Person date={props.items[1].date} name={props.items[1].name} age={props.items[1].age}></Person>
             <Person date={props.items[2].date} name={props.items[2].name} age={props.items[2].age}></Person>
